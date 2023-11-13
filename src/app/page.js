e08aa791de2ee2bead5d7531e9f8cdffef3898d2
PatricioZarauz <pwzarauz@gmail.com>
2023-11-13 00:35:12 -0300

    Added provider index page redirects

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,15 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/options";
 import { redirect } from "next/navigation";
+import { roles } from "@/constants";
 
 const Home = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session) return (redirect('/login'));
+
+  if (session.user.role == roles.proveedor) return (redirect('/providerForm'));
+
   return (redirect('/search'));
 };
 
